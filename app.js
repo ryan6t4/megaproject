9ecdefd7d756446d8551bfd8cfa272a1e3f73ab2
ryan6t4 <mdrayhanh585@gmail.com>
2025-08-24 18:10:05 +0600

    database bug

--- app.js
+++ app.js
@@ -113,6 +113,26 @@ app.use((req, res, next) => {
   res.status(404).send("Route not found!");
 });
 
+// this is added for seed
+const initData = require("./init/data.js");
+
+app.get("/seed", async (req, res) => {
+  try {
+    const count = await Listing.countDocuments();
+    if (count === 0) {
+      await Listing.insertMany(initData);
+      console.log("Seed data inserted");
+      res.send("✅ Data initialized!");
+    } else {
+      console.log("Seed skipped, data already exists");
+      res.send("⚠️ Data already exists. Skipping seed.");
+    }
+  } catch (err) {
+    console.error("Error seeding data:", err);
+    res.status(500).send("Error seeding data");
+  }
+});
+
 // app.listen(8000, () => {
 //   console.log("Server is running on port 8000");
 // });
