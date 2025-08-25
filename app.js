973222f95b56f5c49ae894648d1de6dfa85cd55e
ryan6t4 <mdrayhanh585@gmail.com>
2025-08-25 12:30:51 +0600

    beginning

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 
-const MONGO_URL =
-  "mongodb+srv://internetshrooms3:o8MxVGqJbH5y0Mq4@cluster0.sdcxemx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
 //mongodb connection
 main()
   .then(() => {
@@ -113,31 +113,6 @@ app.use((req, res, next) => {
   res.status(404).send("Route not found!");
 });
 
-// this is added for seed
-const initData = require("./init/data.js");
-
-app.get("/seed", async (req, res) => {
-  try {
-    const count = await Listing.countDocuments();
-    if (count === 0) {
-      await Listing.insertMany(initData);
-      console.log("Seed data inserted");
-      res.send("✅ Data initialized!");
-    } else {
-      console.log("Seed skipped, data already exists");
-      res.send("⚠️ Data already exists. Skipping seed.");
-    }
-  } catch (err) {
-    console.error("Error seeding data:", err);
-    res.status(500).send("Error seeding data");
-  }
-});
-
-// app.listen(8000, () => {
-//   console.log("Server is running on port 8000");
-// });
-
-const PORT = process.env.PORT || 8000; // Render sets PORT automatically
-app.listen(PORT, () => {
-  console.log(`✅ Server running on port ${PORT}`);
+app.listen(8000, () => {
+  console.log("Server is running on port 8000");
 });
--- init/index.js
+++ init/index.js
@@ -2,10 +2,7 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
-
-const MONGO_URL =
-  "mongodb+srv://internetshrooms3:o8MxVGqJbH5y0Mq4@cluster0.sdcxemx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 main()
   .then(() => {
